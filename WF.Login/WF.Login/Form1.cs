01021bebd26d7f071d840b953c805372f6b71dc4
Erick Staub <erickstaubfelipe@gmail.com>
2025-04-10 21:35:38 -0300

    update 1.0 - chaveiro Renato

--- WF.Login/WF.Login/Form1.cs
+++ WF.Login/WF.Login/Form1.cs
@@ -6,5 +6,53 @@ namespace WF.Login
         {
             InitializeComponent();
         }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            string url = "https://shopee.com.br/buyer/reset";
+            
+            
+                MessageBox.Show(url);
+            
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Senha errada");
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "USUARIO")
+                textBox1.Text = string.Empty;
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+                textBox1.Text = "USUARIO";
+        }
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "SENHA")
+                textBox2.Text = string.Empty;
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "")
+                textBox2.Text = "SENHA";
+        }
     }
 }
